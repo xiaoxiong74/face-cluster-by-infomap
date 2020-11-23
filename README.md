8cadc082c189332c5be09547b4f52021def9e054
xiongyongfu <xyf_0704@sina.com>
2020-11-23 14:52:44 +0800

    v1

---
+++ README.md
@@ -0,0 +1,48 @@
+# Learning to Cluster Faces by Infomap
+
+## Infomap Intorduction
+[Infomap Website](https://www.mapequation.org/publications.html#Rosvall-Axelsson-Bergstrom-2009-Map-equation)
+
+## Requirements
+* Python >= 3.6
+* sklearn
+* infomap
+* numpy
+
+## Datasets
+MS-Celeb-1M : part1_test (584K)
+[download](https://github.com/yl-1993/learn-to-cluster/blob/master/DATASET.md)
+
+## Run
+```bash
+python face-cluster-by-infomap
+```
+
+## Results on part1_test (584K)
+| Method | Precision | Recall | F-score |
+| ------ |:---------:|:------:|:-------:|
+| Chinese Whispers (k=80, th=0.6, iters=20) | 55.49 | 52.46 | 53.93 |
+| Approx Rank Order (k=80, th=0) | 99.77 | 7.2 | 13.42 |
+| MiniBatchKmeans (ncluster=5000, bs=100) | 45.48 | 80.98 | 58.25 |
+| KNN DBSCAN (k=80, th=0.7, eps=0.25, min=1) | 95.25 | 52.79 | 67.93 |
+| FastHAC (dist=0.72, single) | 92.07 | 57.28 | 70.63 |
+| [DaskSpectral](https://ml.dask.org/clustering.html#spectral-clustering) (ncluster=8573, affinity='rbf') | 78.75 | 66.59 | 72.16 |
+| [CDP](https://github.com/XiaohangZhan/cdp) (single model, th=0.7)  | 80.19 | 70.47 | 75.02 |
+| [L-GCN](https://github.com/yl-1993/learn-to-cluster/tree/master/lgcn) (k_at_hop=[200, 10], active_conn=10, step=0.6, maxsz=300)  | 74.38 | 83.51 | 78.68 |
+| GCN-D (2 prpsls) | 95.41 | 67.77 | 79.25 |
+| GCN-D (5 prpsls) | 94.62 | 72.59 | 82.15 |
+| GCN-D (8 prpsls) | 94.23 | 79.69 | 86.35 |
+| GCN-D (20 prplss) | 94.54 | 81.62 | 87.61 |
+| GCN-D + GCN-S (2 prpsls) | 99.07 | 67.22 | 80.1 |
+| GCN-D + GCN-S (5 prpsls) | 98.84 | 72.01 | 83.31 |
+| GCN-D + GCN-S (8 prpsls) | 97.93 | 78.98 | 87.44 |
+| GCN-D + GCN-S (20 prpsls) | 97.91 | 80.86 | 88.57 |
+| GCN-V | 92.45 | 82.42 | 87.14 |
+| GCN-V + GCN-E | 92.56 | 83.74 | 87.93 |
+| Infomap(ours) | 95.50 | 92.51 | 93.98 |
+
+![avatar](./image/evaluate.png)
+
+## References
+[最小熵原理（五）：“层层递进”之社区发现与聚类](https://spaces.ac.cn/archives/7006)
+[人脸聚档主流方案](https://github.com/yl-1993/learn-to-cluster)
---
+++ face-cluster-by-infomap.py
@@ -0,0 +1,248 @@
+
+import numpy as np
+from tqdm import tqdm
+import infomap
+import time
+from multiprocessing.dummy import Pool as Threadpool
+from multiprocessing import Pool
+import multiprocessing as mp
+import os
+from utils import Timer
+from evaluation import evaluate, accuracy
+
+
+def l2norm(vec):
+    """
+    归一化
+    :param vec: 
+    :return: 
+    """
+    vec /= np.linalg.norm(vec, axis=1).reshape(-1, 1)
+    return vec
+
+
+def intdict2ndarray(d, default_val=-1):
+    arr = np.zeros(len(d)) + default_val
+    for k, v in d.items():
+        arr[k] = v
+    return arr
+
+
+def read_meta(fn_meta, start_pos=0, verbose=True):
+    """
+    idx2lb：每一个顶点对应一个类
+    lb2idxs：每个类对应一个id
+    """
+    lb2idxs = {}
+    idx2lb = {}
+    with open(fn_meta) as f:
+        for idx, x in enumerate(f.readlines()[start_pos:]):
+            lb = int(x.strip())
+            if lb not in lb2idxs:
+                lb2idxs[lb] = []
+            lb2idxs[lb] += [idx]
+            idx2lb[idx] = lb
+
+    inst_num = len(idx2lb)
+    cls_num = len(lb2idxs)
+    if verbose:
+        print('[{}] #cls: {}, #inst: {}'.format(fn_meta, cls_num, inst_num))
+    return lb2idxs, idx2lb
+
+
+class knn():
+    def __init__(self, feats, k, index_path='', verbose=True):
+        pass
+
+    def filter_by_th(self, i):
+        th_nbrs = []
+        th_dists = []
+        nbrs, dists = self.knns[i]
+        for n, dist in zip(nbrs, dists):
+            if 1 - dist < self.th:
+                continue
+            th_nbrs.append(n)
+            th_dists.append(dist)
+        th_nbrs = np.array(th_nbrs)
+        th_dists = np.array(th_dists)
+        return th_nbrs, th_dists
+
+    def get_knns(self, th=None):
+        if th is None or th <= 0.:
+            return self.knns
+        # TODO: optimize the filtering process by numpy
+        # nproc = mp.cpu_count()
+        nproc = 1
+        with Timer('filter edges by th {} (CPU={})'.format(th, nproc),
+                   self.verbose):
+            self.th = th
+            self.th_knns = []
+            tot = len(self.knns)
+            if nproc > 1:
+                pool = mp.Pool(nproc)
+                th_knns = list(
+                    tqdm(pool.imap(self.filter_by_th, range(tot)), total=tot))
+                pool.close()
+            else:
+                th_knns = [self.filter_by_th(i) for i in range(tot)]
+            return th_knns
+
+
+class knn_faiss_gpu(knn):
+    def __init__(self,
+                 feats,
+                 k,
+                 index_path='',
+                 verbose=True):
+        import faiss
+        with Timer('[faiss_gpu] build index {}'.format(k), verbose):
+            knn_ofn = index_path + '.npz'
+            if os.path.exists(knn_ofn):
+                print('[faiss_gpu] read knns from {}'.format(knn_ofn))
+                self.knns = np.load(knn_ofn)['data']
+            else:
+                feats = feats.astype('float32')
+                size, dim = feats.shape
+                res = faiss.StandardGpuResources()
+                res.setTempMemory(1 * 1024 * 1024 * 1024)
+                index = faiss.GpuIndexFlatIP(res, dim)
+                index.add(feats)
+        with Timer('[faiss_gpu] query topk {}'.format(k), verbose):
+            knn_ofn = index_path + '.npz'
+            if os.path.exists(knn_ofn):
+                pass
+            else:
+                sims, nbrs = index.search(feats, k=k)
+                # torch.cuda.empty_cache()
+                self.knns = [(np.array(nbr, dtype=np.int32),
+                              1 - np.array(sim, dtype=np.float32))
+                             for nbr, sim in zip(nbrs, sims)]
+
+
+def knns2ordered_nbrs(knns, sort=True):
+    if isinstance(knns, list):
+        knns = np.array(knns)
+    nbrs = knns[:, 0, :].astype(np.int32)
+    dists = knns[:, 1, :]
+    if sort:
+        # sort dists from low to high
+        nb_idx = np.argsort(dists, axis=1)
+        idxs = np.arange(nb_idx.shape[0]).reshape(-1, 1)
+        dists = dists[idxs, nb_idx]
+        nbrs = nbrs[idxs, nb_idx]
+    return dists, nbrs
+
+
+# 构造边
+def get_links(single, links, nbrs, dists):
+    for i in tqdm(range(nbrs.shape[0])):
+        count = 0
+        for j in range(0, len(nbrs[i])):
+            # 排除本身节点
+            if i == nbrs[i][j]:
+                pass
+            elif dists[i][j] <= 1 - min_sim:
+                count += 1
+                links[(i, nbrs[i][j])] = float(1 - dists[i][j])
+            else:
+                break
+        # 统计孤立点
+        if count == 0:
+            single.append(i)
+    return single, links
+
+
+def cluster_by_infomap(nbrs, dists, pred_lable_path):
+    """
+    基于infomap的聚类
+    :param nbrs: 
+    :param dists: 
+    :param pred_lable_path: 
+    :return: 
+    """
+    single = []
+    links = {}
+    with Timer('get links', verbose=True):
+        single, links = get_links(single=single, links=links, nbrs=nbrs, dists=dists)
+
+    infomapWrapper = infomap.Infomap("--two-level --directed")
+    for (i, j), sim in tqdm(links.items()):
+        _ = infomapWrapper.addLink(int(i), int(j), sim)
+
+    # 聚类运算
+    infomapWrapper.run()
+
+    label2idx = {}
+    idx2label = {}
+
+    # 聚类结果统计
+    for node in infomapWrapper.iterTree():
+        # node.physicalId 特征向量的编号
+        # node.moduleIndex() 聚类的编号
+        idx2label[node.physicalId] = node.moduleIndex()
+        if node.moduleIndex() not in label2idx:
+            label2idx[node.moduleIndex()] = []
+        label2idx[node.moduleIndex()].append(node.physicalId)
+
+    node_count = 0
+    for k, v in label2idx.items():
+        if k == 0:
+            node_count += len(v[2:])
+            # print(k, v[2:])
+        else:
+            node_count += len(v[1:])
+            # print(k, v[1:])
+
+    print(node_count)
+    # 孤立点个数
+    print(len(single))
+
+    keys_len = len(list(label2idx.keys()))
+    print(keys_len)
+
+    # 孤立点放入到结果中
+    for single_node in single:
+        idx2label[single_node] = keys_len
+        label2idx[keys_len] = [single_node]
+        keys_len += 1
+
+    print(keys_len)
+
+    idx_len = len(list(idx2label.keys()))
+    print(idx_len)
+
+    # 保存结果
+    # with open(pred_lable_path, 'w') as of:
+    #     for idx in range(idx_len):
+    #         of.write(str(idx2label[idx]) + '\n')
+
+    pred_labels = intdict2ndarray(idx2label)
+    true_lb2idxs, true_idx2lb = read_meta(label_path)
+    gt_labels = intdict2ndarray(true_idx2lb)
+    for metric in metrics:
+        evaluate(gt_labels, pred_labels, metric)
+
+
+def get_dist_nbr(feature_path, k=80):
+    features = np.fromfile(feature_path, dtype=np.float32)
+    features = features.reshape(-1, 256)
+    features = l2norm(features)
+
+    index = knn_faiss_gpu(feats=features, k=k)
+    knns = index.get_knns()
+    dists, nbrs = knns2ordered_nbrs(knns)
+    return dists, nbrs
+
+metrics = ['pairwise', 'bcubed', 'nmi']
+min_sim = 0.58
+k = 50
+# true_label
+label_path = '/home/deeplearn/project/learn-to-cluster/data/labels/part1_test.meta'
+feature_path = '/home/deeplearn/project/learn-to-cluster/data/features/part1_test.bin'
+pred_lable_path = '/home/deeplearn/project/learn-to-cluster/evaluate_part/result/part1_test_predict.txt'
+dists, nbrs = get_dist_nbr(feature_path=feature_path, k=k)
+print(dists.shape, nbrs.shape)
+
+cluster_by_infomap(nbrs, dists, pred_lable_path)
+
+
